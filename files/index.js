c79bc37c617f66c83ca4fc0abdcaf414fe3866b9
sharil12 <muhd_sharil@hotmail.com>
2023-11-25 11:47:09 +0800

    amended index.js

--- files/index.js
+++ files/index.js
@@ -1,14 +1,10 @@
 console.log('Loading function');
 
 export const handler = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: "Hello, World!",
-            },
-          null,
-          2
-        ),
-    };
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+    console.log('value1 =', event.key1);
+    console.log('value2 =', event.key2);
+    console.log('value3 =', event.key3);
+    return event.key1;  // Echo back the first key value
+    // throw new Error('Something went wrong');
 };
